1bff9128d86e87f45711fefb67622f98719eaba9
Igor <ikk@mail.ru>
2021-11-06 12:39:36 +0300

    add more tests and styles mock

--- __tests__/App.test.jsx
+++ __tests__/App.test.jsx
@@ -1,19 +1,57 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import { render, screen, cleanup } from '@testing-library/react';
 import { Provider } from 'react-redux';
+import userEvent from '@testing-library/user-event';
 import App from '../src/App';
 import store from '../src/store/store';
 
-it('Виден главный экран приложения', () => {
+afterEach(() => cleanup());
+test('Виден главный экран приложения', () => {
   const app = (
     <Provider store={store}>
       <App />
     </Provider>
   );
-
+  // screen.logTestingPlaygroundURL();
   const { getByRole } = render(app);
 
   const header = getByRole('heading', { name: /your todo list/i });
 
   expect(header).toBeInTheDocument();
+});
+
+test('Добавление новой задачи', () => {
+  const app = (
+    <Provider store={store}>
+      <App />
+    </Provider>
+  );
+  const { getByRole } = render(app);
+  const input = getByRole('textbox');
+  const button = getByRole('button', { name: /add/i });
+  userEvent.type(input, 'Task 1');
+  userEvent.click(button);
+  const task = getByRole('button', { name: /task 1/i });
+  expect(task).toBeInTheDocument();
+  expect(task).toHaveTextContent('Task 1');
+  userEvent.click(getByRole('button', { name: /×/i }));
+  // screen.logTestingPlaygroundURL();
+});
+
+test('Удаление задачи', () => {
+  const app = (
+    <Provider store={store}>
+      <App />
+    </Provider>
+  );
+  const { getByRole } = render(app);
+  const input = getByRole('textbox');
+  const buttonAdd = getByRole('button', { name: /add/i });
+  userEvent.type(input, 'Task 1');
+  userEvent.click(buttonAdd);
+  screen.logTestingPlaygroundURL();
+  const task = getByRole('button', { name: /task 1/i });
+  const buttonDelete = getByRole('button', { name: /×/i });
+  userEvent.click(buttonDelete);
+  expect(task).not.toBeInTheDocument();
 });
