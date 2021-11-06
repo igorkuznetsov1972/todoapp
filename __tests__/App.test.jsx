750a75bb8730cd59a8304ecc0a11690b6b10b4b8
Igor <ikk@mail.ru>
2021-11-06 16:15:30 +0300

    recreate store to avoid mutual test pollution

--- __tests__/App.test.jsx
+++ __tests__/App.test.jsx
@@ -2,56 +2,79 @@ import React from 'react';
 import { render, screen, cleanup } from '@testing-library/react';
 import { Provider } from 'react-redux';
 import userEvent from '@testing-library/user-event';
+import { configureStore } from '@reduxjs/toolkit';
 import App from '../src/App';
-import store from '../src/store/store';
+import taskNameSliceReducer from '../src/slices/taskName';
+import tasksSliceReducer from '../src/slices/tasks';
+import filterSliceReducer from '../src/slices/filter';
 
-afterEach(() => cleanup());
-test('Виден главный экран приложения', () => {
-  const app = (
-    <Provider store={store}>
-      <App />
-    </Provider>
-  );
-  // screen.logTestingPlaygroundURL();
-  const { getByRole } = render(app);
 
-  const header = getByRole('heading', { name: /your todo list/i });
+describe('ToDo App test', () => {
+  test('Виден главный экран приложения', async () => {
+    const store = configureStore({
+      reducer: {
+        taskName: taskNameSliceReducer,
+        tasks: tasksSliceReducer,
+        filter: filterSliceReducer,
+      },
+    });
+    const app = (
+      <Provider store={store}>
+        <App />
+      </Provider>
+    );
+    render(app);
 
-  expect(header).toBeInTheDocument();
-});
+    const header = await screen.getByRole('heading', { name: /your todo list/i });
 
-test('Добавление новой задачи', () => {
-  const app = (
-    <Provider store={store}>
-      <App />
-    </Provider>
-  );
-  const { getByRole } = render(app);
-  const input = getByRole('textbox');
-  const button = getByRole('button', { name: /add/i });
-  userEvent.type(input, 'Task 1');
-  userEvent.click(button);
-  const task = getByRole('button', { name: /task 1/i });
-  expect(task).toBeInTheDocument();
-  expect(task).toHaveTextContent('Task 1');
-  userEvent.click(getByRole('button', { name: /×/i }));
-  // screen.logTestingPlaygroundURL();
-});
+    expect(header).toBeInTheDocument();
+  });
 
-test('Удаление задачи', () => {
-  const app = (
-    <Provider store={store}>
-      <App />
-    </Provider>
-  );
-  const { getByRole } = render(app);
-  const input = getByRole('textbox');
-  const buttonAdd = getByRole('button', { name: /add/i });
-  userEvent.type(input, 'Task 1');
-  userEvent.click(buttonAdd);
-  screen.logTestingPlaygroundURL();
-  const task = getByRole('button', { name: /task 1/i });
-  const buttonDelete = getByRole('button', { name: /×/i });
-  userEvent.click(buttonDelete);
-  expect(task).not.toBeInTheDocument();
+  test('Добавление новой задачи', async () => {
+    const store = configureStore({
+      reducer: {
+        taskName: taskNameSliceReducer,
+        tasks: tasksSliceReducer,
+        filter: filterSliceReducer,
+      },
+    });
+    const app = (
+      <Provider store={store}>
+        <App />
+      </Provider>
+    );
+    render(app);
+    const input = await screen.getByRole('textbox');
+    const button = await screen.getByRole('button', { name: /add/i });
+    userEvent.type(input, 'Task 1');
+    userEvent.click(button);
+    const task = await screen.getByRole('button', { name: /task 1/i });
+    expect(task).toBeInTheDocument();
+    expect(task).toHaveTextContent('Task 1');
+  });
+
+  test('Удаление задачи', async () => {
+    const store = configureStore({
+      reducer: {
+        taskName: taskNameSliceReducer,
+        tasks: tasksSliceReducer,
+        filter: filterSliceReducer,
+      },
+    });
+    const app = (
+      <Provider store={store}>
+        <App />
+      </Provider>
+    );
+    render(app);
+    const input = await screen.getByRole('textbox');
+    const buttonAdd = await screen.getByRole('button', { name: /add/i });
+    userEvent.type(input, 'Task 1');
+    userEvent.click(buttonAdd);
+    // screen.logTestingPlaygroundURL();
+    const task = await screen.getByRole('button', { name: /task 1/i });
+    const buttonDelete = await screen.getByRole('button', { name: /×/i });
+    userEvent.click(buttonDelete);
+    expect(task).not.toBeInTheDocument();
+  });
 });
